05e6b865da13bfe75375061a926095c9d092ebb2
러리 <doralife12@naver.com>
2020-12-24 23:49:15 +0900

    Add: 003

---
+++ node/003/car.js
@@ -0,0 +1,8 @@
+class Car {
+    constructor(name, size) {
+        this.run = () => console.log(`${size}의 크기를 가진 ${name} 자동차가 달립니다.`);
+        this.stop = () => console.log(`${size}의 크기를 가진 ${name} 자동차가 멈췄습니다.`);
+    }
+}
+
+module.exports = Car;
---
+++ node/003/main.js
@@ -0,0 +1,4 @@
+const Car = require('./car');
+
+let car = new Car('안녕', '라지');
+car.run();
