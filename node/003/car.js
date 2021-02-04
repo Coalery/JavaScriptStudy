c0c1fa7b742f2b265b38270f770a6310c0245ae3
러리 <doralife12@naver.com>
2021-02-04 17:04:08 +0900

    Reset

--- node/003/car.js
+++
@@ -1,8 +0,0 @@
-class Car {
-    constructor(name, size) {
-        this.run = () => console.log(`${size}의 크기를 가진 ${name} 자동차가 달립니다.`);
-        this.stop = () => console.log(`${size}의 크기를 가진 ${name} 자동차가 멈췄습니다.`);
-    }
-}
-
-exports.Car = Car;
--- node/003/main.js
+++
@@ -1,4 +0,0 @@
-const Car = require('./car').Car;
-
-let car = new Car('안녕', '라지');
-car.run();
