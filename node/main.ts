439d2dcf919d9af3b200519205627fa8e72dfcd1
러리 <doralife12@naver.com>
2021-02-07 00:58:13 +0900

    Add: Tab 4

--- node/main.ts
+++ node/main.ts
@@ -1,31 +1,31 @@
 class Animal {
-  name: string;
-  constructor(theName: string) {
-    this.name = theName;
-  }
-  move(distance: number = 0) {
-    console.log(`${this.name} moved ${distance}m.`);
-  }
+    name: string;
+    constructor(theName: string) {
+        this.name = theName;
+    }
+    move(distance: number = 0) {
+        console.log(`${this.name} moved ${distance}m.`);
+    }
 }
 
 class Snake extends Animal {
-  constructor(name: string) {
-    super(name);
-  }
-  move(distance: number = 5) {
-    console.log("Slithering ...");
-    super.move(distance);
-  }
+    constructor(name: string) {
+        super(name);
+    }
+    move(distance: number = 5) {
+        console.log("Slithering ...");
+        super.move(distance);
+    }
 }
 
 class Horse extends Animal {
-  constructor(name: string) {
-    super(name);
-  }
-  move(distance: number = 45) {
-    console.log("Galloping...");
-    super.move(distance);
-  }
+    constructor(name: string) {
+        super(name);
+    }
+    move(distance: number = 45) {
+        console.log("Galloping...");
+        super.move(distance);
+    }
 }
 
 let sam = new Snake("Sammy the Python");
