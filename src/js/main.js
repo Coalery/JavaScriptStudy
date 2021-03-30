1eaeae4ed95ff5b6b74d3640349c6106f65de6eb
러리 <doralife12@naver.com>
2021-03-31 00:27:13 +0900

    Feat: wtf is that

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,4 @@ const circle1 = new Circle(1);
 console.log(Object.getPrototypeOf(circle1) === Circle.prototype);
 console.log(circle1.constructor === Circle);
 console.log(Circle.prototype.constructor === Circle);
+console.log(new new new circle1.constructor(5).constructor(10).constructor(15));
