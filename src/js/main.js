16134b52f5ef045e68861d6803c871e19b755d96
러리 <doralife12@naver.com>
2021-04-01 23:50:46 +0900

    Feat: Static property

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,19 @@
-const Person = (function () {
-  function Person(name) {
-    this.name = name;
-  }
+function Person(name) {
+  this.name = name;
+}
 
-  Person.prototype = {
-    constructor: Person,
-    sayHello() {
-      console.log(`Hi! My name is ${this.name}.`);
-    },
-  };
+Person.prototype.sayHello = function () {
+  console.log(`Hi! My name is ${this.name}.`);
+};
 
-  return Person;
-})();
+Person.staticProp = "static prop";
+
+Person.staticMethod = function () {
+  console.log("staticMethod");
+};
 
 const me = new Person("Lee");
 
-console.log(me.constructor === Person);
-console.log(me.constructor === Object);
-console.log(me instanceof Person);
-console.log(me instanceof Object);
+Person.staticMethod();
+// me.staticMethod();
+// TypeError: me.staicMethod is not a function
