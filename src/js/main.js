93253976fa63a65ac90950373ad7ec705a633968
러리 <doralife12@naver.com>
2021-04-06 16:14:22 +0900

    Feat: bind

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
-(function (a) {
-  "use strict";
-  a = 2;
-  console.log(arguments);
-  console.log(a);
-})(1);
+const person = {
+  name: "Lee",
+  foo(callback) {
+    setTimeout(callback.bind(this), 100);
+  },
+};
+
+person.foo(function () {
+  console.log(`Hi! my name is ${this.name}.`);
+});
