cda11ad4c394b77faa3cf4b5cda3708bee54188d
러리 <doralife12@naver.com>
2021-05-07 02:04:19 +0900

    Feat: reduce

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,9 @@
-class Hello {
-  constructor(cb) {
-    this.hi = 5;
-    this.cb = cb;
-    this.cb2 = () => this.hi;
-  }
+const fruits = ["banana", "apple", "orange", "orange", "apple"];
 
-  hello() {
-    return this.cb();
-  }
+const count = fruits.reduce((acc, cur, idx, _) => {
+  acc[cur] = acc[cur] ?? 0;
+  acc[cur]++;
+  return acc;
+}, {});
 
-  hello2() {
-    return this.cb2();
-  }
-}
-
-let hello = new Hello(function () {
-  return this.hi;
-});
-console.log(hello.hello()); // 5
-console.log(hello.hello2()); // 5
-console.log(hello.cb()); // 5
-console.log(hello.cb2()); // 5
-console.log(hello);
-console.log("");
-
-let hello2 = new Hello(() => {
-  return this.hi;
-});
-console.log(hello2.hello()); // undefined
-console.log(hello2.hello2()); // 5
-console.log(hello2.cb()); // undefined
-console.log(hello2.cb2()); // 5
-console.log(hello2);
+console.log(count);
