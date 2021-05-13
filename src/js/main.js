01011ce94f01cd06479a0b7c5b51215f61a3cc2f
러리 <doralife12@naver.com>
2021-05-14 01:45:06 +0900

    Feat: String methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
 const id = "doralife12@naver.com";
 
-const regExp =
-  /^[A-Za-z0-9]+([\._-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\._-]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
-
-console.log(regExp.test(id));
+console.log(id.split("").reverse().join(""));
